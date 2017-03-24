6fd1d01af104c58ad7e26cecaae9c1c5b25c5a8b
Miki <Miki@192.168.3.5>
2017-03-24 13:19:47 -0600

    el de la semana de configgen

--- src/ConfigGen.java
+++ src/ConfigGen.java
@@ -1,4 +1,7 @@
 import java.awt.Image;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import javax.swing.ImageIcon;
 
@@ -10,5 +13,19 @@ public class ConfigGen {
 		Image oim = im.getScaledInstance(width,height,java.awt.Image.SCALE_SMOOTH);
 		ic=new ImageIcon(oim);
 		return ic;
+	}
+	public static int WeekOfYear(String fecha)
+	{
+		//------------- Sacar Semana del mes
+   	 SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
+        java.util.Date fec = null;
+        try {
+            fec = formato.parse(fecha);
+        } catch (ParseException ex) {System.out.println("error al convertir fecha"); return 0; }
+     
+   	Calendar cal=Calendar.getInstance();
+   	cal.setTime(fec);
+   	return cal.get(cal.WEEK_OF_YEAR)-1;
+   	//-------------------------------------------
 	}
 }
